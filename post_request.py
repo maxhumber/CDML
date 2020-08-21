fdb063e8716d42178836bfbbbe0cc88b0a044b66
Max Humber <max.humber@gmail.com>
2020-08-21 18:11:21 -0400

    get rid of intermediate files

--- post_request.py
+++ post_request.py
@@ -2,19 +2,24 @@ import json
 from urllib.request import Request, urlopen
 import pandas as pd
 
-data = json.dumps({
+
+def post(url, data):
+    data = bytes(json.dumps(data).encode("utf-8"))
+    request = Request(
+        url=url,
+        data=data,
+        method="POST"
+    )
+    request.add_header("Content-type", "application/json; charset=UTF-8")
+    with urlopen(request) as response:
+        data = json.loads(response.read().decode("utf-8"))
+    return data
+
+data = {
     "date": str(pd.Timestamp('now')),
-    "temperature": -30
-})
+    "temperature": 20
+}
 
-request = Request(
-    url='http://localhost:5000',
-    data=bytes(data.encode("utf-8")),
-    method="POST"
-)
+post("http://127.0.0.1:5000", data)
 
-request.add_header("Content-type", "application/json; charset=UTF-8")
-
-with urlopen(request) as response:
-    data = json.loads(response.read().decode("utf-8"))
-    print(data)
+post("http://178.128.234.34", data)
