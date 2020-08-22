9e71ef0bbc89bf78699c718c6ddd5be31ba09599
Max Humber <max.humber@gmail.com>
2020-08-22 12:25:14 -0400

    🤞

--- clean.py
+++ clean.py
@@ -3,3 +3,5 @@ import subprocess
 subprocess.run(
     "rm -rf .DS_Store app.py model.py pipe.pkl mapper.pkl model.h5 model.pkl requirements.txt runtime.txt Procfile",
 shell=True)
+
+subprocess.run("git remote rm dokku", shell=True)
