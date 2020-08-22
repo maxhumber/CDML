3296d2ec58f5f8fdaf63fea40a77e00f8ea7ec8e
Max Humber <max.humber@gmail.com>
2020-08-22 12:49:46 -0400

    delete venv

--- clean.py
+++ clean.py
@@ -1,7 +1,7 @@
 import subprocess
 
 subprocess.run(
-    "rm -rf .DS_Store app.py model.py pipe.pkl mapper.pkl model.h5 model.pkl requirements.txt runtime.txt Procfile",
+    "rm -rf .DS_Store .venv app.py model.py pipe.pkl mapper.pkl model.h5 model.pkl requirements.txt runtime.txt Procfile",
 shell=True)
 
 subprocess.run("git remote rm dokku", shell=True)
