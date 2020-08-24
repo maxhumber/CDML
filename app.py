f2b2f39847ccb4fa3622a4991a7d7500ef94f3a8
Max Humber <max.humber@gmail.com>
2020-08-24 13:17:27 -0400

    deploy 2

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 import pickle
-from flask import Flask, request
 import pandas as pd
 
-app = Flask(__name__)
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
 
 with open("pipe.pkl", 'rb') as f:
     pipe = pickle.load(f)
 
-@app.route("/")
+@app.get("/")
 def index():
     return 'Use the /predict endpoint'
 
-@app.route("/predict")
-def predict():
-    query = request.args
-    temperature = float(query.get('temperature'))
+@app.get("/predict")
+def predict(temperature: float):
     new = pd.DataFrame({'temperature': [temperature]})
     prediction = pipe.predict(new)[0]
     return {'prediction': prediction}
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    uvicorn.run(app)
